8cab24f4396a5692d6235d9bf1b72dd4a35e6bb8
Jhef <jdcr26@hotmail.com>
2023-09-16 21:56:21 +0000

    prevent default

--- src/components/Form/form.js
+++ src/components/Form/form.js
@@ -4,27 +4,33 @@ import TextField from "../TextField/TextField"
 import OptionList from "../OptionList/OptionList"
 import Button from '../Button/Button';
 const Form = (props) => {
-	return (
-		<section className='Form'>
-			<form>
-				<h2>Fill in form to add collaborators</h2>
-				<TextField
-					titulo='Name'
-					placeholder={'Type your name'}
-				/>
-				<TextField
-					titulo='Position'
-					placeholder={'Type your position'}
-				/>
-				<TextField
-					titulo='Picture'
-					placeholder='Upload your picture'
-				/>
-				<OptionList />
-				<Button text="Create collaborator"/>
-			</form>
-		</section>
-	);
+    
+    const handleSubmit = (event) => {
+			event.preventDefault();
+			return console.log('handle Submit', event);
+		};
+
+		return (
+			<section className='Form'>
+				<form onSubmit={handleSubmit}>
+					<h2>Fill in form to add collaborators</h2>
+					<TextField
+						titulo='Name'
+						placeholder={'Type your name'}
+					/>
+					<TextField
+						titulo='Position'
+						placeholder={'Type your position'}
+					/>
+					<TextField
+						titulo='Picture'
+						placeholder='Upload your picture'
+					/>
+					<OptionList />
+					<Button text='Create collaborator' />
+				</form>
+			</section>
+		);
 };
 
 
