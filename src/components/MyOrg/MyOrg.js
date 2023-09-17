919be23c4f613b5a67d0a8ab7f2d2913b8759bd2
Jhef <jdcr26@hotmail.com>
2023-09-17 21:56:27 +0000

    Update MyOrg.js

--- src/components/MyOrg/MyOrg.js
+++ src/components/MyOrg/MyOrg.js
@@ -2,15 +2,19 @@ import { useState } from 'react';
 import '../MyOrg/MyOrg.css';
 
 const MyOrg = () => {
-	console.log(useState());
+	console.log();
+
+	// const [name, updateName] = useState("Jheferson")
+	const [name, updateName] = useState(true)
 
 	const handleClick = () => {
 		console.log('Show/Hide element');
+		updateName(!name)
 	};
 
 	return (
 		<section className='orgSection'>
-			<h3 className='title'>My organizatin</h3>
+			<h3 className='title'>My organization {name} </h3>
 			<img
 				onClick={handleClick}
 				src='/img/add-btn.svg'
